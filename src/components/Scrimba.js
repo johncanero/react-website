a445aee376b98be7ce79368f06f4bfa5e28881a6
John Canero <johncanero.official@gmail.com>
2022-08-24 15:27:15 +0800

    Inputting Heading Tags and Lists

--- src/components/Scrimba.js
+++ src/components/Scrimba.js
@@ -2,18 +2,15 @@ const Scrimba = () => {
   
   return (
     <div>
-        <div className="App">
-           <h1>Hello Scrimba</h1>
-        </div>
-        
-        <nav>
-            <h1>Bob's Bistro</h1>
-            <ul>
-                <li>Menu</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
-        </nav>
+        <h1>My awesome website in React</h1>
+        <h3>Reasons I love React</h3>
+
+        <ol>
+          <li>It's composable</li>
+          <li>It's declarative</li>
+          <li>It's hireable skill</li>
+          <li>It's actively mained by skilled people</li>
+        </ol>
     </div> 
   )
 }
