18cd45ccf7b480767bfff1ec0925615277eb821b
John Canero <johncanero.official@gmail.com>
2022-08-24 23:49:20 +0800

    Fic Stylings and Contents, Add a Sticky Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div>
         <footer>
-            <p>© 2022 Jam development. All rights reserved.</p>
+            <p>© Scrimba. All rights reserved.</p>
         </footer>
     </div>
   )
