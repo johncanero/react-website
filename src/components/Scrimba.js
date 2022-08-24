a574a58d7f3af311d6fab1944a542778b0c2adb4
John Canero <johncanero.official@gmail.com>
2022-08-24 13:30:30 +0800

    Create a File: Scrimba.js, Input Contents from Scrimba, Import Scrimba.js at App.js

---
+++ src/components/Scrimba.js
@@ -0,0 +1,21 @@
+const Scrimba = () => {
+  
+  return (
+    <div>
+        <div className="App">
+           <h1>Hello Scrimba</h1>
+        </div>
+        
+        <nav>
+            <h1>Bob's Bistro</h1>
+            <ul>
+                <li>Menu</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    </div> 
+  )
+}
+
+export default Scrimba
