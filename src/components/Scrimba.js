c0fedef89f5dc9be1ad39fd05ae59a82ca9e1a69
John Canero <johncanero.official@gmail.com>
2022-08-24 15:43:29 +0800

    Fun Facts About React and Incuding PNG logo

--- src/components/Scrimba.js
+++ src/components/Scrimba.js
@@ -1,16 +1,21 @@
+import reactlogo from '../images/react-logo.png'
+
 const Scrimba = () => {
   
   return (
     <div>
-        <h1>My awesome website in React</h1>
-        <h3>Reasons I love React</h3>
+        <div>
+           <img className='reactLogo' src={reactlogo} alt="reactlogo"/>
+        </div>
+        <h1>Fun Facts About React</h1>
 
-        <ol>
-          <li>It's composable</li>
-          <li>It's declarative</li>
-          <li>It's hireable skill</li>
-          <li>It's actively mained by skilled people</li>
-        </ol>
+        <ul>
+          <li>Was released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100k stars on Github</li>
+          <li>Is maintaned by Facebook</li>
+          <li>Powers thousands of enterpise apps, including mobile apps</li>
+        </ul>
     </div> 
   )
 }
