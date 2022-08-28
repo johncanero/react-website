cae58c5e44fb8e971d482143762a1993d81a303c
John Canero <johncanero.official@gmail.com>
2022-08-28 18:18:34 +0800

    Update Spacing

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -6,7 +6,6 @@ const MainContent = () => {
     <div>
         <main>
             <h1 className='main--title'>Fun Facts About React</h1>
-            
             <ul className='main--facts'>
               <li>Was released in 2013</li>
               <li>Was originally created by Jordan Walke</li>
