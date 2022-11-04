f1b20add1b81cb6ac247411c67ce22edf36acbea
John Canero <johncanero.official@gmail.com>
2022-11-04 14:30:04 +0800

    Import react from react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import './App.css';
-import React, { Component }  from 'react';
+
 
 function App() {
   return (
