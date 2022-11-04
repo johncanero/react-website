230e7258a752c74d83f75940793543be9a9c2a06
John Canero <johncanero.official@gmail.com>
2022-11-04 14:29:29 +0800

    Import React Component and Install React Save for Rendering

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import './App.css';
+import React, { Component }  from 'react';
 
 function App() {
   return (
